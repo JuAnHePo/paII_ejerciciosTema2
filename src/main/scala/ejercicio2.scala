bc763123f32f59dcb8f17066f52673fc43dd53ce
JuAnHePo <juanantonioherreraponferrada@gmail.com>
2025-03-23 15:53:31 +0100

    ejercicio2.scala programmed and tested

--- src/main/scala/ejercicio2.scala
+++ src/main/scala/ejercicio2.scala
@@ -1,3 +1,23 @@
 object ejercicio2 {
+  def comprimirCadena(cadena: String): String = {
+    def codificar(cadena: String, c: Char): String = {
+      var cnt = 0
+      for (i <- 0 until cadena.length)
+        if c == cadena.charAt(i) then cnt += 1
+      c.toString + cnt
+    }
 
+    val cadenaComprimida = StringBuilder("")
+    for (i <- 0 until cadena.length)
+      if !cadenaComprimida.contains(cadena.charAt(i)) then
+        cadenaComprimida.append(codificar(cadena, cadena.charAt(i)))
+    cadenaComprimida.toString()
+  }
+
+  @main def mainComprimirCadena(): Unit = {
+    var cadena = "aaabcc"
+    println(s"La cadena inicial es $cadena.")
+    cadena = comprimirCadena(cadena)
+    println(s"La cadena comprimida es $cadena.\n")
+  }
 }
