b667b1638106bc7b0b8b80d85d1fbe7fc87e78c6
JuAnHePo <juanantonioherreraponferrada@gmail.com>
2025-03-25 12:16:34 +0100

    ejercicio9.scala programmed and tested

--- src/main/scala/ejercicio9.scala
+++ src/main/scala/ejercicio9.scala
@@ -1,3 +1,16 @@
-object ejercicio9 {
+import scala.io.StdIn.readLine
+import scala.util.Try
 
+object ejercicio9 {
+  def leer(): Try[Int] = {
+    print("Introduzca un número entero: ")
+    Try(readLine().toInt)
+  }
+  @main def mainScan(): Unit = {
+    var n = leer()
+    while n.isFailure do
+      print("Valor introducido inválido. ho")
+      n = leer()
+    println(s"El número introducido es: ${n.get}")
+  }
 }
