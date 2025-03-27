b3724724493ba320b54660fbf665d17b7c2fe8d0
JuAnHePo <juanantonioherreraponferrada@gmail.com>
2025-03-27 17:00:39 +0100

    ejercicio20.scala programmed and tested

--- src/main/scala/ejercicio20.scala
+++ src/main/scala/ejercicio20.scala
@@ -1,3 +1,15 @@
 object ejercicio20 {
+  def sumaCuadrados(lista: List[Int]): Int = {
+    def go(lista: List[Int], acc: Int): Int = {
+      if lista.isEmpty then acc
+      else go(lista.tail, acc + lista.head)
+    }
 
+    go(lista, 0)
+  }
+
+  @main def mainSumaCuadrados(): Unit = {
+    val lista = 1 :: 2 :: 3 :: 4 :: Nil
+    println(s"La suma de los elementos de la lista $lista es: ${sumaCuadrados(lista)}.")
+  }
 }
